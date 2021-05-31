31ddd13401426893ec5be9e5df5acd979dfb5cec
Degubi <gergonoorbi@gmail.com>
2021-05-31 12:24:03 +0200

    Add publish files

--- build.py
+++ build.py
@@ -1,12 +1,6 @@
 from subprocess import call
-from os import remove, rename, environ
+from os import environ
 
 JAVA_HOME = environ.get('JAVA_HOME')
 
-call('"C:/Program Files (x86)/Microsoft Visual Studio/2019/Community/VC/Auxiliary/Build/vcvars64.bat" && ' +
-    f'cl /LD /MD /Od /I "{JAVA_HOME}/include" /I "{JAVA_HOME}/include/win32" MediaProps.cpp ole32.lib Shell32.lib')
-
-remove('MediaProps.obj')
-remove('MediaProps.exp')
-remove('MediaProps.lib')
-print('\nDone!')
+call(f'cl /LD /MD /Od /I "{JAVA_HOME}/include" /I "{JAVA_HOME}/include/win32" MediaProps.cpp ole32.lib Shell32.lib')
